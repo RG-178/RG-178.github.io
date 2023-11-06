8b5cbd785ddc9db213166a3e35e0a8c3a79e8cbc
Riccardo Ambühl <riccardo.ambuehl@icloud.com>
2023-11-06 19:35:22 +0100

    update

--- test.js
+++ test.js
@@ -1,33 +1,116 @@
-// Funktion zum Hinzufügen einer Aufgabe zur Liste
+// Lade die JSON-Datei vom Server
+const JSON_URL = 'tasks.json'; // Hier ersetzen Sie die URL durch den tatsächlichen Speicherort Ihrer JSON-Datei
+
+function loadTasks() {
+    fetch(JSON_URL)
+        .then(response => response.json())
+        .then(data => {
+            const taskList = document.getElementById("taskList");
+            taskList.innerHTML = ''; // Leere die aktuelle Liste
+
+            data.tasks.forEach((taskText, index) => {
+                const listItem = document.createElement("li");
+
+                const taskTextElement = document.createElement("span");
+                taskTextElement.textContent = taskText;
+                listItem.appendChild(taskTextElement);
+
+                const editButton = document.createElement("button");
+                editButton.textContent = "Bearbeiten";
+                editButton.onclick = () => editTask(index, taskText);
+                listItem.appendChild(editButton);
+
+                const deleteButton = document.createElement("button");
+                deleteButton.textContent = "Löschen";
+                deleteButton.onclick = () => deleteTask(index);
+                listItem.appendChild(deleteButton);
+
+                taskList.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error(error));
+}
+
+loadTasks(); // Lade Aufgaben beim Start der Seite
+
 function addTask() {
     const taskInput = document.getElementById("task");
-    const taskList = document.getElementById("taskList");
     const taskText = taskInput.value;
 
     if (taskText) {
-        // Aufgabe zur Liste hinzufügen
-        const listItem = document.createElement("li");
-        listItem.textContent = taskText;
-        taskList.appendChild(listItem);
-
-        // Aufgabe im LocalStorage speichern
-        const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-        tasks.push(taskText);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-
-        // Textfeld leeren
-        taskInput.value = "";
+        fetch(JSON_URL)
+            .then(response => response.json())
+            .then(data => {
+                data.tasks.push(taskText);
+                return fetch(JSON_URL, {
+                    method: 'PUT', // Sie müssen den richtigen HTTP-Verb entsprechend konfigurieren
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                });
+            })
+            .then(() => {
+                loadTasks(); // Aktualisiere die Liste nach dem Hinzufügen
+                taskInput.value = ''; // Leere das Eingabefeld
+            })
+            .catch(error => console.error(error));
     }
 }
 
-// Aufgaben aus dem LocalStorage beim Laden der Seite wiederherstellen
-window.addEventListener("load", function() {
-    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-    const taskList = document.getElementById("taskList");
+function editTask(index, oldTaskText) {
+    const newTaskText = prompt("Aufgabe bearbeiten:", oldTaskText);
 
-    tasks.forEach(function(taskText) {
-        const listItem = document.createElement("li");
-        listItem.textContent = taskText;
-        taskList.appendChild(listItem);
-    });
-});
+    if (newTaskText !== null) {
+        fetch(JSON_URL)
+            .then(response => response.json())
+            .then(data => {
+                data.tasks[index] = newTaskText;
+                return fetch(JSON_URL, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                });
+            })
+            .then(() => {
+                loadTasks(); // Aktualisiere die Liste nach der Bearbeitung
+            })
+            .catch(error => console.error(error));
+    }
+}
+
+function deleteTask(index) {
+    fetch(JSON_URL)
+        .then(response => response.json())
+        .then(data => {
+            data.tasks.splice(index, 1);
+            return fetch(JSON_URL, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+        })
+        .then(() => {
+            loadTasks(); // Aktualisiere die Liste nach dem Löschen
+        })
+        .catch(error => console.error(error));
+}
+
+function clearAllTasks() {
+    if (confirm("Möchten Sie alle Aufgaben löschen?")) {
+        fetch(JSON_URL, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(() => {
+                loadTasks(); // Aktualisiere die Liste nach dem Löschen
+            })
+            .catch(error => console.error(error));
+    }
+}
