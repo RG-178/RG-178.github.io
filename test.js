36dd6d162cd8d31fdeb39b90c414bb4fdf406970
Riccardo Ambühl <riccardo.ambuehl@icloud.com>
2023-11-06 18:42:43 +0100

    update

--- test.js
+++ test.js
@@ -1,36 +1,33 @@
-function displayTasks() {
-    const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
-    const list = document.getElementById('todo-list');
-    list.innerHTML = '';
+// Funktion zum Hinzufügen einer Aufgabe zur Liste
+function addTask() {
+    const taskInput = document.getElementById("task");
+    const taskList = document.getElementById("taskList");
+    const taskText = taskInput.value;
 
-    todoList.forEach(task => {
-        const li = document.createElement('li');
-        li.textContent = task;
-        list.appendChild(li);
-    });
+    if (taskText) {
+        // Aufgabe zur Liste hinzufügen
+        const listItem = document.createElement("li");
+        listItem.textContent = taskText;
+        taskList.appendChild(listItem);
+
+        // Aufgabe im LocalStorage speichern
+        const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        tasks.push(taskText);
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+
+        // Textfeld leeren
+        taskInput.value = "";
+    }
 }
 
-// Funktion, um eine Aufgabe hinzuzufügen
-document.getElementById('add-task').addEventListener('click', () => {
-    const taskInput = document.getElementById('task');
-    const task = taskInput.value.trim();
+// Aufgaben aus dem LocalStorage beim Laden der Seite wiederherstellen
+window.addEventListener("load", function() {
+    const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    const taskList = document.getElementById("taskList");
 
-    if (task !== '') {
-        const todoList = JSON.parse(localStorage.getItem('todoList')) || [];
-        todoList.push(task);
-        localStorage.setItem('todoList', JSON.stringify(todoList));
-        taskInput.value = '';
-        displayTasks();
-    }
-});
-
-// Funktion, um alle Aufgaben zu löschen
-document.getElementById('clear-tasks').addEventListener('click', () => {
-    localStorage.removeItem('todoList');
-    displayTasks();
-});
-
-// Seite lädt, Anzeige der To-Do-Liste
-window.addEventListener('load', () => {
-    displayTasks();
+    tasks.forEach(function(taskText) {
+        const listItem = document.createElement("li");
+        listItem.textContent = taskText;
+        taskList.appendChild(listItem);
+    });
 });
